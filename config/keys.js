5bc5ff543c3c988539b72aedc51a119b582a542e
Quaei <51792380+Quaei@users.noreply.github.com>
2019-06-27 18:27:43 +0200

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = { // replace 'test' with database name                       !!
+    MongoURI: 'mongodb+srv://quaei:edendvatri123@cluster-1-gcsmd.mongodb.net/test?retryWrites=true&w=majority'
+}
